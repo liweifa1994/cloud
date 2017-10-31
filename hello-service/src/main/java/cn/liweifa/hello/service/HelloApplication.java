2df1401e7232275db54389a97e5dd74a747ce858
liweifa1994 <2414270508@qq.com>
2017-11-01 07:59:06 +0800

    Initial commit

---
+++ hello-service/src/main/java/cn/liweifa/hello/service/HelloApplication.java
@@ -0,0 +1,14 @@
+package cn.liweifa.hello.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}

cfe6dc6dccbda0142847c8cd2fddbb0cfbdfa626
liweifa1994 <2414270508@qq.com>
2017-11-01 23:35:49 +0800

    config-service config-client

---
+++ config-client/src/main/java/cn/liweifa/config/ConfigClentApplication.java
@@ -0,0 +1,13 @@
+package cn.liweifa.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClentApplication.class,args);
+    }
+}
